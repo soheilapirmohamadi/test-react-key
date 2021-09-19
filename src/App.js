586ec1ce448e5fae6441cb1620fd32de6c64c892
soheilapirmohamadi <soheylpirmohamadi@gmail.com>
2021-09-19 16:01:59 +0430

    rrr

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MyComponent from './Components/MyComponent';
 
 function App() {
   return (
-    [1,2,3,4,5,6].map(a=><MyComponent key={a} id={a} />)
+    [1,2,3,4,5,6,8,9,10].map(a=><MyComponent key={a} id={a} />)
   );
 }
 
