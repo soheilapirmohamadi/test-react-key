5023884788f6f2a453fe377cd2db72547f3047be
soheilapirmohamadi <soheylpirmohamadi@gmail.com>
2021-09-19 15:55:53 +0430

    a

---
+++ src/Components/MyComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                salam {this.props.id}
+            </div>
+        )
+    }
+}
